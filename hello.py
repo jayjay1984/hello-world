b7fa5c932a1c7c90cb9d76ae63f59ac4695d4d3d
jayjay1984 <kaiser.joerg@gmx.net>
2020-01-10 12:18:20 +0100

    more

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
 print("Hello World!")
+#print(2 ** 1000)
+
+print("""Customer: Good morning.
+Owner: Good morning, Sir. 
+Welcome to the National Cheese Emporium.""")
+print('Customer: Good morning.\nOwner: Good morning, Sir. Welcome to the National Cheese Emporium.')
